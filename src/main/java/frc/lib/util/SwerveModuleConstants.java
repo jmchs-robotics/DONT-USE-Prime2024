acb83d721688831ede5a413b847823424db209dc
Ian Galaviz <ig25049@judgememorial.com>
2024-01-23 08:40:56 -0700

    Made our own SwerveModule code all on our own (We totally did not steal from Nutmeg)

--- src/main/java/frc/lib/util/SwerveModuleConstants.java
+++ src/main/java/frc/lib/util/SwerveModuleConstants.java
@@ -1,4 +1,4 @@
-package frc.util;
+package frc.lib.util;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 
@@ -6,22 +6,19 @@ public class SwerveModuleConstants {
 
     public final int driveMotorID;
     public final int angleMotorID;
-    public final int cancoderID;
-    public final Rotation2d angleOffset;
+    public final double angleOffset;
 
 
     /**
      * Swerve Module Constants to be used when creating swerve modules.
      * @param driveMotorID
      * @param angleMotorID
-     * @param cancoderID
      * @param angleOffset
      */
-    public SwerveModuleConstants(int driveMotorID, int angleMotorID, int cancoderID, Rotation2d angleOffset) {
+    public SwerveModuleConstants(int driveMotorID, int angleMotorID, double angleOffset) {
 
         this.driveMotorID = driveMotorID;
         this.angleMotorID = angleMotorID;
-        this.cancoderID = cancoderID;
         this.angleOffset = angleOffset;
 
     }
